41957874fdd3bde2c4b4c14c6f9b89cfac88d51a
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2015-03-10 16:04:11 -0700

    ckpt

--- spec/api/arkivo_spec.rb
+++ spec/api/arkivo_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe Sufia::API do
+describe Sufia::Arkivo do
   describe 'Zotero integration' do
     # POST to http://arkivo.host/api/subscription
-    # { url: “https://api.zotero.org/users/{zotero-id}/my-research/items”, key: “zotero-api-key”, plugins: [{name : “file_repository”, parameters: { user_token: “unique-user-specific-token” }]}
+    # { url: “https://api.zotero.org/users/{zotero-id}/my-research/items”, key: “zotero-api-key”, plugins: [{name : “hydra”, parameters: { user_token: “unique-user-specific-token” }]}
     it 'initiates an OAuth connection to Zotero'
     it 'receives an access token from Zotero'
     # https://api.zotero.org/users/{zotero_id}/my-research/items
@@ -12,7 +12,7 @@ describe Sufia::API do
     # parse subscription URL out of JSON or location header, and store in ScholarSphere, for later possible invalidation via DELETE
     it 'includes a subscription URL'
     it 'fails with bad credentials'
+    # DELETE /api/subscription/:id?invalidate-key=true
     it 'hits the Zotero API with HTTP DELETE if subscription request fails'
-    it 'creates and stores a unique token per user account'
   end
 end
